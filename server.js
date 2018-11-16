80dc8ff35f4f60812e594e54fb5f7c406fc7a65e
NoPlanet_NoParty <eclancy11@gmail.com>
2018-11-15 22:12:28 -0500

    server adjust

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var path = require('path')
-var api = require('./app/routing/apiRoutes')
-var html = require('./app/routing/htmlRoutes')
+var api = require('./FriendFinder/app/routing/apiRoutes')
+var html = require('./FriendFinder/app/routing/htmlRoutes')
 
 var app = express()
 var PORT = process.env.PORT || 8080;
